ebb32c3873837f6896efcd45099ad4a644a97dcd
Md Monir Hossain Showrav <18103142@iubat.edu>
2020-09-26 00:41:03 +0600

    :heavy_plus_sign: Add preloader, and some funtionalities

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+window.addEventListener("load",function(){
+    document.querySelector(".preloader").classList.add("opacity-0");
+    setTimeout(function(){
+        document.querySelector(".preloader").style.display="none";
+    },1000)
+})
+
+
 //Portfolio item filter
 const filterContainer=document.querySelector(".protfolio-filter"),
       filterBtns= filterContainer.children,
@@ -82,13 +90,11 @@ const nav=document.querySelector(".nav"),
 for(let i=0; i<totalNavList; i++){
     const a=navList[i].querySelector("a");
     a.addEventListener("click",function(){
-        for(let i=0;i<totalSection;i++){
-            allSection[i].classList.remove("back-section");
-        }
+        removeBackSectionClass();
+        
         for(let j=0; j<totalNavList; j++){
             if(navList[j].querySelector("a").classList.contains("active")){
-                allSection[j].classList.add("back-section");
-
+                addBackSectionClass(j);
             }
             navList[j].querySelector("a").classList.remove("active");
         }
@@ -101,6 +107,16 @@ for(let i=0; i<totalNavList; i++){
         }
     })
 }
+function removeBackSectionClass(){
+for(let i=0;i<totalSection;i++){
+    allSection[i].classList.remove("back-section"); 
+    }
+}
+function addBackSectionClass(num){
+    allSection[num].classList.add("back-section");
+}
+
+
 function showSection(element){
     for(let i=0;i<totalSection;i++){
         allSection[i].classList.remove("active");
@@ -108,6 +124,27 @@ function showSection(element){
     const target= element.getAttribute("href").split("#")[1];
     document.querySelector("#"+target).classList.add("active")
 }
+
+
+function updateNav(element){
+    for(let i=0; i<totalNavList; i++){
+        navList[i].querySelector("a").classList.remove("active");
+        const target=element.getAttribute("href").split("#")[1];
+        if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+            navList[i].querySelector("a").classList.add("active");
+        }
+    }
+
+}
+
+document.querySelector(".hire-me").addEventListener("click",function(){
+    const sectionIndex=this.getAttribute("data-section-index");
+    showSection(this);
+    updateNav(this);
+    removeBackSectionClass();
+    addBackSectionClass(sectionIndex);
+
+})
 
 const navTogglerBtn=document.querySelector(".nav-toggler"),
       aside=document.querySelector(".aside");    
